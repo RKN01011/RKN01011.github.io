8070fc205077bd07c915d951b5c81ddadb10cdba
RKN <hd9171399@gmail.com>
2019-05-21 17:50:21 +0300

    ;j

---
+++ precache-manifest.2b95a686ce890aca9d46488b21c0ba0e.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c728fe76f627fcc8e26",
+    "url": "/static/css/main.5b460411.chunk.css"
+  },
+  {
+    "revision": "5c728fe76f627fcc8e26",
+    "url": "/static/js/main.96d9bd31.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "219b6567398f1445a51a",
+    "url": "/static/js/2.98f146af.chunk.js"
+  },
+  {
+    "revision": "e6007df3fec0a20bdcc383fcb9a2744c",
+    "url": "/static/media/lolita.e6007df3.jpg"
+  },
+  {
+    "revision": "df7d573397101f1aea480fe013de3c2f",
+    "url": "/static/media/clockwork.df7d5733.jpg"
+  },
+  {
+    "revision": "a6c6f4718294c5cd2cfc8194151c4166",
+    "url": "/static/media/meta.a6c6f471.jpg"
+  },
+  {
+    "revision": "42bf40a7d3728a0dfb54102797530052",
+    "url": "/static/media/b1984.42bf40a7.jpg"
+  },
+  {
+    "revision": "c8dd1f4f3e53f129b2793f5f187e8fb0",
+    "url": "/static/media/alice.c8dd1f4f.jpg"
+  },
+  {
+    "revision": "a0feeba9fdc46547504b3b9ad82d7d87",
+    "url": "/static/media/f451.a0feeba9.jpg"
+  },
+  {
+    "revision": "1c7e976fbb03a34db6ad5a52cae41646",
+    "url": "/static/media/kafka.1c7e976f.jpg"
+  },
+  {
+    "revision": "c041d87846a391d0fdcb74b30ea22a8f",
+    "url": "/static/media/gatsby.c041d878.jpg"
+  },
+  {
+    "revision": "53933be92256dbd727f6515f8193bfcc",
+    "url": "/static/media/oldman.53933be9.jpg"
+  },
+  {
+    "revision": "9cf5ffc7c68d621646af0bbb419e8487",
+    "url": "/static/media/beath.9cf5ffc7.jpg"
+  },
+  {
+    "revision": "6055c0c27093355eff2432bdfe648e82",
+    "url": "/static/media/girlandbook.6055c0c2.jpg"
+  },
+  {
+    "revision": "0c525cd6a92379b44ae937db267c523c",
+    "url": "/static/media/soLittle.0c525cd6.jpg"
+  },
+  {
+    "revision": "434829ff51369a0b11a5ab9521b55ec3",
+    "url": "/static/media/cornerFon.434829ff.jpg"
+  },
+  {
+    "revision": "b4c401cbc7dd76aba42a4a1e72aba4c5",
+    "url": "/static/media/cather.b4c401cb.jpg"
+  },
+  {
+    "revision": "75280c1cc23808ec85caff15e7eacbcd",
+    "url": "/static/media/fixefon.75280c1c.jpeg"
+  },
+  {
+    "revision": "8619084310c41ecf070b8c871fe620cb",
+    "url": "/static/media/frogFon.86190843.jpg"
+  },
+  {
+    "revision": "bd0d824c7ca77cef0eb790fb4bb86409",
+    "url": "/static/media/bookgirl.bd0d824c.jpg"
+  },
+  {
+    "revision": "54806f466c551ee55f1e22fcb763203b",
+    "url": "/static/media/logo.54806f46.png"
+  },
+  {
+    "revision": "b8b5207c01c4a3066cd6b15447249c60",
+    "url": "/static/media/winowGirl.b8b5207c.jpg"
+  },
+  {
+    "revision": "c2c49f6ab1097495448a6f074a860a67",
+    "url": "/static/media/bookBuy.c2c49f6a.jpg"
+  },
+  {
+    "revision": "148595951a81a03093507f2e7e1329ef",
+    "url": "/static/media/classic.14859595.png"
+  },
+  {
+    "revision": "c2b83d20124953deec5dc32538bee8f9",
+    "url": "/static/media/modern.c2b83d20.png"
+  },
+  {
+    "revision": "b4f606c38e8188c384072a9eb125cc49",
+    "url": "/static/media/novel.b4f606c3.png"
+  },
+  {
+    "revision": "fbf8193bb6319aa8f1d75c1a7b43bfc1",
+    "url": "/static/media/scifi.fbf8193b.png"
+  },
+  {
+    "revision": "c1e74252e4649d760479aa868d6bfd2d",
+    "url": "/static/media/facebook.c1e74252.svg"
+  },
+  {
+    "revision": "e67dcdf58486738cee65626e46368f8e",
+    "url": "/static/media/instagram-icon.e67dcdf5.svg"
+  },
+  {
+    "revision": "7013352abab5e10d8b4426c164922072",
+    "url": "/static/media/pinterest.7013352a.svg"
+  },
+  {
+    "revision": "78d7ea6462be839f4a227b6b71b5df97",
+    "url": "/static/media/bookfon.78d7ea64.jpg"
+  },
+  {
+    "revision": "688c203418f172ef0dc5dfc9fa6baab5",
+    "url": "/static/media/about1.688c2034.jpg"
+  },
+  {
+    "revision": "1c673dd999833d7125d6e11c693992fa",
+    "url": "/static/media/bookandgirl.1c673dd9.jpg"
+  },
+  {
+    "revision": "43b4887bd20d706be4d269f967d5e826",
+    "url": "/static/media/ookandminigirl.43b4887b.jpg"
+  },
+  {
+    "revision": "fdd352593be46c1fd4013399d7445345",
+    "url": "/static/media/about2.fdd35259.jpg"
+  },
+  {
+    "revision": "d891fe557b8ce66f0ac7debe7e606e89",
+    "url": "/static/media/bookandman.d891fe55.jpg"
+  },
+  {
+    "revision": "94f0d9e642af81e36b62943e10522a99",
+    "url": "/static/media/abotHeder.94f0d9e6.jpg"
+  },
+  {
+    "revision": "952616f994ff0fa53a08dbbc266a542b",
+    "url": "/static/media/contact.952616f9.jpg"
+  },
+  {
+    "revision": "f764f0b56c39ebc779ac083ff5510f18",
+    "url": "/static/media/Oswaldregular.f764f0b5.woff"
+  },
+  {
+    "revision": "561373b3d3f9b5c95597a37559a34544",
+    "url": "/static/media/Oswaldlight.561373b3.woff"
+  },
+  {
+    "revision": "50b1abf51d57e6796cbe48346ecf4d6f",
+    "url": "/static/media/Oswaldregular.50b1abf5.woff2"
+  },
+  {
+    "revision": "0fa6098c1987b3829363b3a793ba9288",
+    "url": "/static/media/Oswaldlight.0fa6098c.woff2"
+  },
+  {
+    "revision": "79dfeb9c9b35aa2b4cf9bf6f2ebf7da8",
+    "url": "/static/media/Oswaldlight.79dfeb9c.ttf"
+  },
+  {
+    "revision": "4647c1d949e48ef298602d6f23da49cf",
+    "url": "/static/media/Oswaldregular.4647c1d9.ttf"
+  },
+  {
+    "revision": "d9bb6dc89f98a349988d5b1c8df84baf",
+    "url": "/index.html"
+  }
+];
